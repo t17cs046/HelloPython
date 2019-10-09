8e088d3fa05045f496e6989684ed2f072758507d
t17cs046 <t17cs046@yamanashi.ac.jp>
2019-10-09 16:11:02 +0900

    first cmmit

---
+++ hello.py
@@ -0,0 +1,19 @@
+'''
+Created on 2019/10/09
+
+@author: t17cs046
+'''
+print('Hello,Python world!')
+a = 10
+b = 0.000001
+c = 'string'
+d = 10; e = 0.00001; f="string"
+# This is comment
+print(a,b,c)
+for x in {1,2,3}:
+    print(x)
+x = 10
+if 0 < x < 10:
+    print('0<x<10')
+else:
+    print('x<=0, x>=10')
